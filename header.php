4dbb15dfa2550b93a8d6a7dbbb1986154d729437
iromeros <72274684+iromeros@users.noreply.github.com>
2021-06-16 05:13:28 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.83.1">
+  <title>Recuperación</title>
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+  <?php wp_head();?>
+  
+
+</head>
+
+  <body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#opciones">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- logo -->
+    <a class="navbar-brand" href="#">
+      <img src="http://www.tutorialesprogramacionya.com/imagenes/foto1.jpg" width="30" height="30" alt="">
+    </a>
+
+    <!-- enlaces -->
+    <div class="collapse navbar-collapse" id="opciones">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Opción 1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Opción 2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Opción 3</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Opción 4</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
---
+++ index.php
@@ -0,0 +1,186 @@
+<?php
+get_header();
+?>
+
+<body>
+
+  <div class="container">
+    <header class="blog-header py-3">
+      <div class="row flex-nowrap justify-content-between align-items-center">
+        <div class="col-4 pt-1">
+          <a class="link-secondary" href="#">Subscribirse</a>
+        </div>
+        <div class="col-4 text-center">
+          <a class="blog-header-logo text-dark" href="#">Largo</a>
+        </div>
+        <div class="col-4 d-flex justify-content-end align-items-center">
+          <a class="link-secondary" href="#" aria-label="Search">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
+              stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img"
+              viewBox="0 0 24 24">
+              <title>Search</title>
+              <circle cx="10.5" cy="10.5" r="7.5" />
+              <path d="M21 21l-5.2-5.2" />
+            </svg>
+          </a>
+          <a class="btn btn-sm btn-outline-secondary" href="#">Iniciar</a>
+        </div>
+      </div>
+    </header>
+
+    <div class="nav-scroller py-1 mb-2">
+      <nav class="nav d-flex justify-content-between">
+        <a class="p-2 link-secondary" href="#">Mundo</a>
+        <a class="p-2 link-secondary" href="#">U.S.</a>
+        <a class="p-2 link-secondary" href="#">Tecnologia</a>
+        <a class="p-2 link-secondary" href="#">Diseño</a>
+        <a class="p-2 link-secondary" href="#">Cultura</a>
+        <a class="p-2 link-secondary" href="#">Noticias</a>
+        <a class="p-2 link-secondary" href="#">Politica</a>
+        <a class="p-2 link-secondary" href="#">Opinión</a>
+        <a class="p-2 link-secondary" href="#">ciencia</a>
+        <a class="p-2 link-secondary" href="#">Salud</a>
+        <a class="p-2 link-secondary" href="#">Moda</a>
+        <a class="p-2 link-secondary" href="#">Viajes</a>
+      </nav>
+    </div>
+  </div>
+
+  <main class="container">
+    <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
+      <div class="col-md-6 px-0">
+        <h1 class="display-4 fst-italic">Un bonito blog de amistad y amor</h1>
+        <p class="lead my-3">Este contenido no pretende ofender y denigrar a las personas</p>
+        <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continua Leyendo...</a></p>
+      </div>
+    </div>
+
+    <div class="row mb-2">
+      <div class="col-md-6">
+        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-primary">Mundo</strong>
+            <h3 class="mb-0">Salud</h3>
+            <div class="mb-1 text-muted">Feb 30</div>
+            <p class="card-text mb-auto">Lo que veras acontinuación puede herir tu sensibilidad si eres subsceptible a
+              los rayos Ultravioleta</p>
+            <a href="#" class="stretched-link">Continua leyendo...</a>
+          </div>
+          <div class="col-auto d-none d-lg-block">
+            <img src="./img/137.png">
+
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-success">Diseño</strong>
+            <h3 class="mb-0">Noticia relevante</h3>
+            <div class="mb-1 text-muted">Nov 11</div>
+            <p class="mb-auto">El one piece sigue sin encontrarse</p>
+            <a href="#" class="stretched-link">Continuar Leyendo...</a>
+          </div>
+          <div class="col-auto d-none d-lg-block">
+            <img src="img/images.jpg" </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-5">
+        <div class="col-md-8">
+          <h3 class="pb-4 mb-4 fst-italic border-bottom">
+            Era de esperar la verdad
+          </h3>
+
+          <article class="blog-post">
+            <h2 class="blog-post-title">Parte importante del blog</h2>
+            <p class="blog-post-meta">January 1, 2021 by <a href="#">Iván</a></p>
+
+            <p>Tortilla de patatas ingredientes 4 personas:</p>
+
+            <p>Primer paso: cortar y freír las patatas y la cebolla,Pelamos y lavamos las patatas, las cortamos en
+              rodajas finas al igual que la cebolla.
+              Segundo paso: mezclar con los huevos, Las sacamos de la sartén y escurrimos bien. Ponemos en un cuenco
+              grande, aparte batimos los huevos y los añadimos a las patatas y a la cebolla,Tercer paso: cuajar la
+              tortilla
+              Ponemos en la sartén un par de cucharadas de aceite de oliva virgen extra y vertemos todo. Al principio
+              rompemos un poco, como si fuéramos a hacer un revuelto. </p>
+
+            <h2>Resumen Evangelión</h2>
+            <p>Neon Genesis Evangelion (新世紀エヴァンゲリオン, Shin Seiki Evangerion ?), también conocida simplemente como
+              Evangelion,​ o Eva,​​ es una serie de anime creada por el estudio Gainax y dirigida por Hideaki Anno.
+
+              La historia de la obra se da lugar en un mundo futurista en el que una organización paramilitar llamada
+              NERV protege a la humanidad de los ataques de seres de origen y naturaleza desconocidos, los «ángeles»,
+              utilizando para ello bio mechas humanoides llamados Evangelion (acortado a EVA).
+
+              A medida que avanza la trama, esta se vuelve paulatinamente más confusa y psicológica, en donde las
+              personalidades de los personajes se hacen cada vez más inestables y su desarrollo se torna fundamental.
+
+              Hideaki Anno, el director y guionista, ha expresado que el desarrollo de la serie estuvo inspirado en su
+              propia experiencia, en virtud de la cual los personajes muestran una amplia gama de sus afecciones
+              emocionales y de su personalidad.
+
+              El anime cuenta con veintiséis episodios que fueron transmitidos por primera vez entre octubre de 1995 y
+              marzo de 1996. Debido a la fama obtenida, pronto se fueron creando diversos spin-offs.
+
+              Esta serie ha sido clasificada en los géneros ciencia ficción, mecha y distopía, conteniendo diversos
+              elementos de filosofía, psicología y religión, con marcadas influencias abrahámicas. Además, sus
+              características técnicas y temáticas, así como la complejidad y simbolismo de su historia, han hecho que
+              esta producción sea considerada como uno de los mejores ejemplos del género del realismo épico. También ha
+              recibido algunos de los mayores premios de animación. Por todo ello, Evangelion es considerada por muchos
+              como una de las producciones más grandes del anime.</p>
+
+          </article>
+
+          <nav class="blog-pagination" aria-label="Pagination">
+            <a class="btn btn-outline-primary" href="#">Antiguos</a>
+            <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Nuevos</a>
+          </nav>
+
+        </div>
+
+        <div class="col-md-4">
+          <div class="position-sticky" style="top: 2rem;">
+            <div class="p-4 mb-3 bg-light rounded">
+              <h4 class="fst-italic">Sobre</h4>
+              <p class="mb-0">2.2.2. “He cometido muchos errores en mi vida. Pero ninguno de ellos me ha hecho sentir
+                tan mal como este. Me arrepiento de haberte causado tanto dolor. Por favor, perdóname”</p>
+            </div>
+
+            <div class="p-4">
+              <h4 class="fst-italic">Archivos</h4>
+              <ol class="list-unstyled mb-0">
+                <li><a href="#">Marzo 2021</a></li>
+                <li><a href="#">Febrero 2021</a></li>
+                <li><a href="#">Enero 2021</a></li>
+                <li><a href="#">Deciembre 2020</a></li>
+                <li><a href="#">Noviembre 2020</a></li>
+                <li><a href="#">Octubre 2020</a></li>
+                <li><a href="#">Septiembre 2020</a></li>
+                <li><a href="#">Agosto 2020</a></li>
+                <li><a href="#">Julio 2020</a></li>
+                <li><a href="#">Junio 2020</a></li>
+                <li><a href="#">Mayo 2020</a></li>
+                <li><a href="#">Abril 2020</a></li>
+              </ol>
+            </div>
+
+            <div class="p-4">
+              <h4 class="fst-italic">Extras</h4>
+              <ol class="list-unstyled">
+                <li><a href="#">GitHub</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Facebook</a></li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+
+  </main>
+
+  <?php
+get_footer();
+?>
